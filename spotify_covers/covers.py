baaadf09047f01b0538405a8978ff353620e4926
gordonjb <gordonjb@users.noreply.github.com>
2020-06-17 12:55:47 +0100

    Add flake8 linter to dev-deps, make code compliant

--- spotify_covers/covers.py
+++ spotify_covers/covers.py
@@ -3,21 +3,24 @@
 from PIL import Image
 from tomlkit import loads
 from spotify_covers.utils import get_project_root
-import sys, os
+import sys
+import os
+
 
 def main():
-    doc = loads(open(os.path.join(get_project_root(), 'config\\covers.toml')).read())
-    
+    doc = loads(open(
+        os.path.join(get_project_root(), 'config\\covers.toml')).read())
+
     for cover in doc['cover']:
         try:
-            cover_image = Image.open(os.path.join(get_project_root(), 'images', cover['bg-image']))
+            cover_image = Image.open(
+                os.path.join(get_project_root(), 'images', cover['bg-image']))
 
         except IOError:
             print("Unable to load image")
             sys.exit(1)
-    
+
         cover_image.show()
-    
 
 
 if __name__ == "__main__":
