778d6da6bdb8118a0dcb861f78c48fb1b2289683
gordonjb <gordonjb@users.noreply.github.com>
2020-06-19 01:54:12 +0100

    Get watermark working correctly
    The opacity level may need tweaks. It looks perfect for white bgs, but
    barely visible on photo backgrounds.

--- spotify_covers/covers.py
+++ spotify_covers/covers.py
@@ -9,28 +9,49 @@ import os
 
 logo_padding_percentage = 3.865
 logo_size_percentage = 7.4
-logo_transparency_percentage = 50
+logo_transparency_percentage = 15
 
 
 def get_logo_location(output_size):
     logo_padding_ratio = logo_padding_percentage/100
-    print(logo_padding_ratio)
     padding_px = int(logo_padding_ratio * output_size[0])
-    print(padding_px)
     return (padding_px, padding_px)
 
 
 def get_logo_size(output_size):
     logo_size_ratio = logo_size_percentage/100
-    print(logo_size_ratio)
     size_px = int(logo_size_ratio * output_size[0])
-    print(size_px)
-    print(logo_size_ratio * output_size[0])
     return (size_px, size_px)
 
 
 def get_logo_alpha_int():
     return int((logo_transparency_percentage * 255)/100)
+
+
+def get_logo_alpha_percent():
+    return logo_transparency_percentage/100
+
+
+def get_scaled_size(output_size, scale_percentage):
+    scale_ratio = scale_percentage/100
+    size_px = int(scale_ratio * output_size[0])
+    return (size_px, size_px)
+
+
+def resize_and_fade_logo(logo, logo_size, output_size, padded_logo_location):
+    logo = ImageOps.fit(logo, logo_size, Image.ANTIALIAS)
+    base = Image.new('RGBA', output_size, (0, 0, 0, 0))
+    base.paste(logo, padded_logo_location, logo)
+    base = Image.blend(Image.new('RGBA', output_size, (0, 0, 0, 0)), base, get_logo_alpha_percent())
+    return base
+
+
+def scale_and_pad_cover(cover_image, output_size, scale):
+    padded = ImageOps.pad(cover_image, get_scaled_size(output_size, scale), Image.ANTIALIAS)
+    base = Image.new('RGBA', output_size, (0, 0, 0, 0))
+    s_width, s_height = padded.size
+    base.paste(padded, (int(output_size[0]/2 - s_width/2), int(output_size[0]/2 - s_height/2)), padded)
+    return base
 
 
 def get_test_image_1():
@@ -59,41 +80,56 @@ def get_cover_image(cover):
 
 def main():
     doc = loads(open(
-        os.path.join(get_project_root(), 'config', 'covers.toml')).read())
+        os.path.join(get_project_root(), 'config', 'covers.toml'), ).read())
 
     output_size = (doc['config']['output-size'], doc['config']['output-size'])
 
     logo_size = get_logo_size(output_size)
     padded_logo_location = get_logo_location(output_size)
-    logo_alpha = get_logo_alpha_int()
 
-    spotify_logo_w = get_white_logo()
-    spotify_logo_w = ImageOps.fit(spotify_logo_w, logo_size, Image.ANTIALIAS)
-    spotify_logo_w.putalpha(logo_alpha)
-    spotify_logo_b = get_black_logo()
-    spotify_logo_b = ImageOps.fit(spotify_logo_b, logo_size, Image.ANTIALIAS)
-    spotify_logo_b.putalpha(logo_alpha)
+    w_base = resize_and_fade_logo(get_white_logo(), logo_size, output_size, padded_logo_location)
+    b_base = resize_and_fade_logo(get_black_logo(), logo_size, output_size, padded_logo_location)
 
     test_image = get_test_image_1()
     test_image = ImageOps.fit(
                 test_image, output_size, Image.ANTIALIAS)
+    test_image.putalpha(255)
+    test_image.show()
 
     for cover in doc['cover']:
         try:
             cover_image = get_cover_image(cover)
-            cover_image = ImageOps.fit(
-                cover_image, output_size, Image.ANTIALIAS)
-            if cover.get('use-black-logo'):
-                cover_image.paste(spotify_logo_b, padded_logo_location, spotify_logo_b)
+
+            # If scale defined, resize cover to scaled size, using pad to
+            # avoid any cropping, otherwise scale to output size to fit
+            if cover.get('scale'):
+                cover_image = scale_and_pad_cover(cover_image, output_size, cover.get('scale'))
             else:
-                cover_image.paste(spotify_logo_w, padded_logo_location, spotify_logo_w)
+                cover_image = ImageOps.fit(
+                    cover_image, output_size, Image.ANTIALIAS)
+
+            # Add alpha channel so mode matches images with transparency,
+            # allowing us to use alpha_composite
+            if cover_image.mode == 'RGB':
+                cover_image.putalpha(255)
+
+            # If a background colour is defined, create a new image with
+            # that colour, and composite the cover onto it.
+            if cover.get('bg-colour'):
+                base = Image.new('RGBA', output_size, cover.get('bg-colour'))
+                cover_image = Image.alpha_composite(base, cover_image)
+
+            # Composite the logo watermark on top, black logo as default
+            if cover.get('use-white-logo'):
+                cover_image = Image.alpha_composite(cover_image, w_base)
+            else:
+                cover_image = Image.alpha_composite(cover_image, b_base)
 
         except IOError:
             print("Unable to load image")
             sys.exit(1)
 
         cover_image.show()
-        Image.blend(cover_image, test_image, 0.85).show()
 
 
 if __name__ == "__main__":
